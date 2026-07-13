f4cdf35744b7dc7e471e57c3cedfb96880d87318
susmitha4304 <susmitha.karaka07@gmail.com>
2026-07-13 20:25:20 +0530

    Add files via upload

---
+++ Spring REST using Spring Boot/REST - Country Web Service/CountryController.java
@@ -0,0 +1,21 @@
+package spring_learn;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CountryController {
+
+    @RequestMapping("/country")
+    public Country getCountryIndia() {
+
+        ApplicationContext context =
+                new ClassPathXmlApplicationContext("country.xml");
+
+        Country country = context.getBean("india", Country.class);
+
+        return country;
+    }
+}
