66d7f82503bf89ac9792ea0cf079d2de9829f9b1
susmitha4304 <susmitha.karaka07@gmail.com>
2026-07-15 19:57:34 +0530

    Add files via upload

---
+++ Microservices with Spring Boot 3.0 /Creating Microservices for loan/LoanController.java
@@ -0,0 +1,21 @@
+package loan.controller;
+
+import loan.model.Loan;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/loans")
+public class LoanController {
+
+    @GetMapping("/{number}")
+    public Loan getLoan(@PathVariable String number) {
+
+        return new Loan(
+                number,
+                "car",
+                400000,
+                3258,
+                18
+        );
+    }
+}
