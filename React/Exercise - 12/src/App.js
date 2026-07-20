8bd209261e4098c2f07cd623cbedff634a316d40
susmitha4304 <susmitha.karaka07@gmail.com>
2026-07-20 10:21:11 +0530

    Add files via upload

---
+++ React/Exercise - 12/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import LoginControl from './LoginControl';
+
+function App() {
+  return (
+    <div className="App">
+      <LoginControl />
+    </div>
+  );
+}
+
+export default App;
+
